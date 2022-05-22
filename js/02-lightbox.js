53a39943f3d5ef6a30f549862c4719afd88108fd
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-05-22 19:34:42 +0300

    made lightbox gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery')
+}
+
+const galleryMarkup = (obj) => {
+    return galleryItems.reduce((acc, {preview, original, description }) => acc + `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="" title="${description}" />
+</a>`, '') 
+}
+
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup(galleryItems))
+
+new SimpleLightbox('.gallery a', {  captionDelay: 250,});
