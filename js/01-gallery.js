42644aeb3aa2cb629b641e6e1009e9f09743e3f7
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-05-22 11:14:24 +0300

    made 01 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,13 +4,13 @@ import { galleryItems } from './gallery-items.js';
 const refs = {
 gallery: document.querySelector('.gallery')
 }
-
+let modalMarkup = ""
 refs.gallery.addEventListener('click', onGalleryClick)
 
 function makeGalleryMarkup({ preview, original, description }) {
     return `    
     <div class="gallery__item">
-    <a href="" class="gallery__link"     onclick="return false"
+    <a href="" class="gallery__link"     
 
         ><img
         class="gallery__image"
@@ -24,8 +24,35 @@ const galleryMarkup = galleryItems.map(makeGalleryMarkup).join("")
 refs.gallery.insertAdjacentHTML('afterbegin', galleryMarkup)
 
 function onGalleryClick(event) {
+    event.preventDefault();
+
     if (event.target.nodeName !== 'IMG')
     { return }
-    console.log(event.target.dataset.sourse);
+    else {
+        modalMarkup = basicLightbox.create(`
+    <img src="${event.target.dataset.sourse}" width="800" height="600">
+`)
+        modalMarkup.show()
+        addKeyListener()
+            }
+}
 
+const addKeyListener = () =>{
+    if (basicLightbox.visible()) {
+        window.addEventListener('keydown', onEscBtnPress);
+    } return
+}
+ 
+const removeEventListener = () => {
+    window.removeEventListener('keydown',onEscBtnPress);
+}
+
+const onEscBtnPress = (event) => {
+    if (event.key !== "Escape") {
+        return
+    }
+    else {
+        modalMarkup.close();
+        removeEventListener()
+}
 }
